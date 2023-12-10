abe0f953d98180f2346cd8bc68ca6f82271f0271
stellachoi27 <116605476+stellachoi27@users.noreply.github.com>
2023-12-09 19:38:46 -0500

    eggy2

--- eggytranslations23/eggytranslations24/background.js
+++
@@ -1,8 +0,0 @@
-// Create a background script to handle messages from the content script and communicate with the popup:
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.selectedText) {
-      // Communicate the selected text to the popup
-      chrome.runtime.sendMessage({ selectedText: request.selectedText });
-    }
-  });
--- eggytranslations23/eggytranslations25/eggytranslations24/content.js
+++
@@ -1,7 +0,0 @@
-// Modify content.js to capture the selected text and communicate with the popup:
-
-document.addEventListener('mouseup', function () {
-    const selectedText = window.getSelection().toString();
-    chrome.runtime.sendMessage({ selectedText: selectedText });
-  });
-
